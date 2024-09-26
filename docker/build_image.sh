af417e5f089e0c28b938f7ecff201196b4df148a
maximilian-brodbeck <maximilian.brodbeck@isd.uni-stuttgart.de>
2024-09-26 08:56:51 +0200

    Modify build-script for docker

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -2,14 +2,14 @@
 set -e
 CONTAINER_ENGINE="docker"
 
-if [ "$DEQLB_HOME" = "" ];
+if [ "$AFEM_HOME" = "" ];
 then
     # Error if path to dolfinx_eqlb i not set
-    echo "Patch to source folder not set! Use "export DEQLB_HOME=/home/.../dolfinx_eqlb""
+    echo "Patch to source folder not set! Use "export AFEM_HOME=/home/.../AFEM-by-Equilibration""
     exit 1
 else
     # Build docker image
-    echo "DEQLB_HOME is set to '$DEQLB_HOME'"
+    echo "DEQLB_HOME is set to '$AFEM_HOME'"
     ${CONTAINER_ENGINE} pull dolfinx/dolfinx:v0.6.0-r1
-    ${CONTAINER_ENGINE} build --no-cache -f "${DEQLB_HOME}/docker/Dockerfile" -t brodbeck-m/dolfinx_eqlb:release .
+    ${CONTAINER_ENGINE} build --no-cache -f "${AFEM_HOME}/docker/Dockerfile" -t brodbeck-m/afem-by-eqlb:release .
 fi
